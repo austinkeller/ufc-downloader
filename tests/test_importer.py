f62a66351cec56f5d00bc44453ed25eb9eab32b0
Austin Keller <austin474@gmail.com>
2025-07-03 00:51:41 -0700

    Improve matching

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,6 +1,6 @@
 import pytest
 import logging
-from ufc_downloader.importer import verify_match
+from ufc_downloader.importer import prune_edition_from_movie_title, verify_match
 
 
 # Configure logging for tests to capture warnings
@@ -12,53 +12,43 @@ def caplog_fixture(caplog):
 
 
 @pytest.mark.parametrize(
-    "movie_title, event_name, expected_result, expect_warning",
+    "movie_title, event_name, expected_result",
     [
-        # Test cases where numbers match or movie_title has no numbers
-        ("UFC.on.ESPN.69", "UFC on ESPN 69", True, False),
-        ("UFC.299", "UFC 299 OMalley vs Vera 2", True, False),
-        (
-            "UFC.Fight.Night",
-            "UFC Fight Night 234",
-            True,
-            False,
-        ),  # movie_numbers is empty
-        (
-            "UFC.299.Part.2",
-            "UFC 299 OMalley vs Vera 2",
-            True,
-            False,
-        ),  # movie_numbers={299, 2}, event_numbers={299, 2}
-        ("UFC.No.Numbers", "Event.No.Numbers", True, False),  # Both have no numbers
-        ("UFC.100", "UFC 100", True, False),
-        ("UFC.1", "UFC 1", True, False),
-        ("UFC.1.2.3", "UFC 1.2.3.4", True, False),  # movie_numbers is a subset
+        ##############################################################
+        # Test cases where numbers match or event_name has no numbers
+        ##############################################################
+        ("UFC.on.ESPN.69", "UFC on ESPN 69", True),
+        ("UFC.299", "UFC 299 OMalley vs Vera 2", False),
+        # movie_numbers is empty
+        ("UFC.Fight.Night", "UFC Fight Night 234", False),
+        # movie_numbers={299, 2}, event_numbers={299, 2}
+        ("UFC.299.Part.2", "UFC 299 OMalley vs Vera 2", True),
+        # movie title includes a date
+        ("UFC.317.Topuria.Vs.Oliveira.2025-06-28", "UFC 317 Topuria vs Oliveira", True),
+        # Both have no numbers. Not ideal since this would be unexpected but we should
+        # allow it unless it becomes a problem.
+        ("UFC.No.Numbers", "Event.No.Numbers", True),
+        ("UFC.100", "UFC 100", True),
+        ("UFC.1", "UFC 1", True),
+        # event_numbers is a subset
+        ("UFC.1.2.3.4", "UFC 1 2 3", True),
+        ##############################################################
         # Test cases where numbers do not match, expecting a warning and False
-        ("UFC.on.ESPN.69", "UFC on ESPN 68", False, True),  # Mismatch in numbers
-        (
-            "UFC.299.Part.1",
-            "UFC 299 OMalley vs Vera 2",
-            False,
-            True,
-        ),  # movie_numbers={299, 1}, event_numbers={299, 2}
-        (
-            "UFC.299",
-            "UFC.Fight.Night",
-            False,
-            True,
-        ),  # movie_numbers has numbers, event_numbers doesn't
-        (
-            "UFC.100.Part.1",
-            "UFC 100 Part 2",
-            False,
-            True,
-        ),  # Different parts, same main number, but different part numbers
-        ("UFC.1.2.3.4", "UFC 1.2.3", False, True),  # movie_numbers is not a subset
+        ##############################################################
+        # Mismatch in numbers
+        ("UFC.on.ESPN.69", "UFC on ESPN 68", False),
+        # movie_numbers={299, 1}, event_numbers={299, 2}
+        ("UFC.299.Part.1", "UFC 299 OMalley vs Vera 2", False),
+        # In this case we should probably fail to import but we'll let it ride since
+        # this verification only should care about events with numbers
+        ("UFC.299", "UFC.Fight.Night", True),
+        # Different parts, same main number, but different part numbers
+        ("UFC.100.Part.1", "UFC 100 Part 2", False),
+        # event_numbers is not a subset
+        ("UFC.1.2.3", "UFC 1 2 3 4", False),
     ],
 )
-def test_verify_match(
-    movie_title, event_name, expected_result, expect_warning, caplog_fixture
-):
+def test_verify_match(movie_title, event_name, expected_result, caplog_fixture):
     """
     Tests the verify_match function with various movie_title and event_name combinations.
     """
@@ -68,7 +58,7 @@ def test_verify_match(
         result == expected_result
     ), f"Expected {expected_result} but got {result} for movie_title='{movie_title}' and event_name='{event_name}'"
 
-    if expect_warning:
+    if not expected_result:
         assert (
             len(caplog_fixture.records) == 1
         ), f"Expected exactly one warning for '{movie_title}' but found {len(caplog_fixture.records)}."
@@ -79,3 +69,16 @@ def test_verify_match(
         assert (
             len(caplog_fixture.records) == 0
         ), f"Unexpected warning for movie_title='{movie_title}' and event_name='{event_name}'"
+
+
+@pytest.mark.parametrize(
+    "movie_title, expected_result",
+    [
+        ("UFC Fight Night 234 Prelims", "UFC Fight Night 234"),
+        ("UFC Fight Night 234", "UFC Fight Night 234"),
+        ("UFC Fight Night 234 Early Prelims", "UFC Fight Night 234"),
+    ],
+)
+def test_prune_edition_from_movie_title(movie_title, expected_result):
+    result = prune_edition_from_movie_title(movie_title)
+    assert result == expected_result, f"Expected {expected_result} but got {result}"
--- ufc_downloader/importer.py
+++ ufc_downloader/importer.py
@@ -19,20 +19,29 @@ RENAME_FILE_FORMAT = "{movie_title} {{edition-{edition_tag}}}"
 
 
 def find_closest_match(
-    movie_title: str, titles_to_events_map: dict[str, dict]
+    movie_title: str, titles_to_events_map: dict[str, dict], source_path
 ) -> Optional[str]:
     # Find the closest match
     closest_matches = difflib.get_close_matches(
-        movie_title, titles_to_events_map.keys(), n=1, cutoff=0.6
+        movie_title, titles_to_events_map.keys(), n=3, cutoff=0.6
     )
 
     if closest_matches:
-        closest_match = closest_matches[0]
-        logger.info(f"Closest match found: {closest_match}")
-        return closest_match
-    else:
-        logger.error(f"No close match found for the movie title {movie_title}")
-        return None
+        for closest_match in closest_matches:
+            closest_match = closest_matches[0]
+            logger.info(f"Closest match found: {closest_match}")
+            if verify_match(movie_title, closest_match, source_path):
+                return closest_match
+
+    # If the movie title omits names then a match may not be found. Check for strict subset matching
+    for title in titles_to_events_map.keys():
+        if movie_title.lower() in title.lower():
+            logger.info(f"Strict subset match found: {title}")
+            if verify_match(movie_title, title, source_path):
+                return title
+
+    logger.error(f"No close match found for the movie title {movie_title}")
+    return None
 
 
 def verify_match(movie_title: str, event_name: str, source_path: str) -> bool:
@@ -41,11 +50,11 @@ def verify_match(movie_title: str, event_name: str, source_path: str) -> bool:
     movie_numbers = set(re.findall(r"\d+", movie_title))
     event_numbers = set(re.findall(r"\d+", event_name))
 
-    # If the movie title has numbers, they must be a subset of the numbers in the event name.
-    if movie_numbers and not movie_numbers.issubset(event_numbers):
+    # If the event name has numbers, they must be a subset of the numbers in the movie title.
+    if event_numbers and not event_numbers.issubset(movie_numbers):
         logger.warning(
-            f"Potential mismatch for '{source_path}'. Numbers in title "
-            f"({movie_numbers}) do not match numbers in event name "
+            f"Potential mismatch for '{source_path}'. Numbers "
+            f"({movie_numbers}) in title '{movie_title}' do not match numbers in event name "
             f"'{event_name}' ({event_numbers})."
         )
         return False
@@ -79,6 +88,28 @@ def handle_empty_source_dir(
         print(f"Deleted empty directory: {source_path}")
         return True
     return False
+
+
+def prune_edition_from_movie_title(movie_title: str) -> str:
+    tokens = movie_title.split(" ")
+    tokens = [
+        token
+        for token in tokens
+        if "prelims" not in token.lower() and "early" not in token.lower()
+    ]
+    return " ".join(tokens)
+
+
+def classify_edition(movie_title: str) -> str:
+    """
+    Classify the edition as either Main Event, Prelims, or Early Prelims
+    """
+    if "early prelims" in movie_title.lower():
+        return "Early Prelims"
+    elif "prelims" in movie_title.lower():
+        return "Prelims"
+    else:
+        return "Main Event"
 
 
 def import_downloads(
@@ -119,16 +150,13 @@ def import_downloads(
                             break
 
                     movie_title = " ".join(tokens)
-
-                    # Classify the edition as either Main Event, Prelims, or Early Prelims
-                    edition = "Main Event"
-                    if "early prelims" in movie_title.lower():
-                        edition = "Early Prelims"
-                    elif "prelims" in movie_title.lower():
-                        edition = "Prelims"
+                    edition = classify_edition(movie_title)
+                    movie_title = prune_edition_from_movie_title(movie_title)
 
                     # Match to an event
-                    event_name = find_closest_match(movie_title, titles_to_events_map)
+                    event_name = find_closest_match(
+                        movie_title, titles_to_events_map, source_path
+                    )
                     if not event_name or not verify_match(
                         movie_title, event_name, source_path
                     ):
@@ -175,6 +203,25 @@ def import_downloads(
                         continue
                     os.rename(source_path, dest_path)
                     logger.info(f"Moved {source_path} to {dest_path}")
+                    # Rename the movie file to match the directory format
+                    for _, _, files in os.walk(dest_path):
+                        for file in files:
+                            # Get file extension
+                            file_ext = os.path.splitext(file)[1]
+                            # Rename
+                            original_file_path = os.path.join(dest_path, file)
+                            new_file_path = os.path.join(
+                                dest_path, dest_dir_name + file_ext
+                            )
+                            logger.info(
+                                f"Renaming {original_file_path} to {new_file_path}"
+                            )
+                            os.rename(original_file_path, new_file_path)
+                            # Write metadata about the renaming to a .txt file in the destination directory
+                            logger.info(f"Writing rename log to {dest_path}")
+                            file_log = os.path.join(dest_path, file + ".txt")
+                            with open(file_log, "a") as f:
+                                f.write(f"{original_file_path} -> {new_file_path}\n")
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         return False
